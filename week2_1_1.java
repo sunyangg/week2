5fb8fc810989657547b2f10d74bb8d166be8d704
sunyangg <cutegoat20041@hotmail.com>
2014-02-25 14:02:51 +0800

    第二周的作業(更改版)

--- week2_1_1.java
+++ week2_1_1.java
@@ -2,7 +2,7 @@ public class week2_1_1
 {
 	public static void main(String args[])
 	{
-		int seconds=13*54*23-4*23*45;
+		int seconds=(13*60*60+54*60+23)-(4*60*60+23*60+13);
 		System.out.print("\n4點23分45秒到13點54分23秒之間的總秒數: "+seconds+"秒\n");
 	}
 }
--- week2_1_2.java
+++ week2_1_2.java
@@ -3,7 +3,8 @@ public class week2_1_2
 	public static void main(String args[])
 	{
 		int num[]={1,2,3,4,5,6,7,8,9};
-		int sum=0,avg;
+		int sum=0;
+		double avg;
 		for(int i=0;i<num.length;i++)
 		{
 			sum+=num[i];
